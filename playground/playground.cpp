7fc11b805d727a0dff140edbd6625220cf13e926
ABigBigWhale <hao.liu.email@gmail.com>
2020-06-07 23:07:22 -0400

    [5] Free bmp data buffer

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -278,5 +278,7 @@ static GLuint LoadBMP(const char *imgPath) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
+    free(data);
+
     return textureID;
 }
